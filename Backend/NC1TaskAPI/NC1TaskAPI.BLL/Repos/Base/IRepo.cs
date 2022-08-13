dd38ab0d0f5507b0d126237397184a042275ea83
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-13 19:55:26 +0300

    base repo class and interface

---
+++ Backend/NC1TaskAPI/NC1TaskAPI.BLL/Repos/Base/IRepo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NC1TaskAPI.BLL.Repos.Base;
+public interface IRepo<T> : IDisposable where T : class
+{
+    DbSet<T> Table { get; }
+    int Add(T entity, bool persist = true);
+    Task<int> AddAsync(T entity, bool persist = true);
+
+    int AddRange(IEnumerable<T> entities, bool persist = true);
+    Task<int> AddRangeAsync(IEnumerable<T> entities, bool persist = true);
+
+    int Update(T entity, bool persist = true);
+
+    int UpdateRange(IEnumerable<T> entities, bool persist = true);
+
+    int Delete(T entity, bool persist = true);
+
+    int DeleteRange(IEnumerable<T> entities, bool persist = true);
+
+    IEnumerable<T> GetAll();
+
+    int SaveChanges();
+    Task<int> SaveChangesAsync();
+}
---
+++ Backend/NC1TaskAPI/NC1TaskAPI.BLL/Repos/Base/RepoBase.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using NC1TaskAPI.DAL.EF;
+
+namespace NC1TaskAPI.BLL.Repos.Base;
+internal class RepoBase<T> : IRepo<T> where T : class
+{
+    private readonly bool _disposeContext;
+    private bool _isDisposed;
+
+    protected RepoBase(ApplicationContext context)
+    {
+        Context = context ?? throw new ArgumentNullException(nameof(context));
+        Table = Context.Set<T>();
+        _disposeContext = false;
+    }
+
+    public ApplicationContext Context { get; }
+    public DbSet<T> Table { get; }
+
+    public virtual int Add(T entity, bool persist = true)
+    {
+        Table.Add(entity);
+        return persist ? SaveChanges() : 0;
+    }
+    public virtual async Task<int> AddAsync(T entity, bool persist = true)
+    {
+        await Table.AddAsync(entity);
+        return persist ? await SaveChangesAsync() : 0;
+    }
+    public virtual int AddRange(IEnumerable<T> entities, bool persist = true)
+    {
+        Table.AddRange(entities);
+        return persist ? SaveChanges() : 0;
+    }
+    public virtual async Task<int> AddRangeAsync(IEnumerable<T> entities, bool persist = true)
+    {
+        await Table.AddRangeAsync(entities);
+        return persist ? await SaveChangesAsync() : 0;
+    }
+
+    public virtual int Update(T entity, bool persist = true)
+    {
+        Table.Update(entity);
+        return persist ? SaveChanges() : 0;
+    }
+    public virtual int UpdateRange(IEnumerable<T> entities, bool persist = true)
+    {
+        Table.UpdateRange(entities);
+        return persist ? SaveChanges() : 0;
+    }
+
+    public virtual int Delete(T entity, bool persist = true)
+    {
+        Table.Remove(entity);
+        return persist ? SaveChanges() : 0;
+    }
+    public virtual int DeleteRange(IEnumerable<T> entities, bool persist = true)
+    {
+        Table.RemoveRange(entities);
+        return persist ? SaveChanges() : 0;
+    }
+
+    public virtual T? Find(int id)
+    {
+        return Table.Find(id);
+    }
+    public virtual async Task<T?> FindAsync(int id)
+    {
+        return await Table.FindAsync(id);
+    }
+
+    public virtual IEnumerable<T> GetAll()
+    {
+        return Table;
+    }
+
+    public virtual int SaveChanges()
+    {
+        try
+        {
+            return Context.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("An error occurred updating the database", ex);
+        }
+    }
+    public virtual async Task<int> SaveChangesAsync()
+    {
+        try
+        {
+            return await Context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("An error occurred updating the database", ex);
+        }
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_isDisposed)
+            return;
+
+        if (disposing && _disposeContext)
+            Context.Dispose();
+
+        _isDisposed = true;
+    }
+    ~RepoBase()
+    {
+        Dispose(disposing: false);
+    }
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
