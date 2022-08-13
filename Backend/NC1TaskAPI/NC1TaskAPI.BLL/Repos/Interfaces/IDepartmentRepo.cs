1f71ba4a8c9ebd37e638da8f0d2ef52700daae5c
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-13 20:00:17 +0300

    entities repos interfaces

---
+++ Backend/NC1TaskAPI/NC1TaskAPI.BLL/Repos/Interfaces/IDepartmentRepo.cs
@@ -0,0 +1,7 @@
+﻿using NC1TaskAPI.BLL.Repos.Base;
+using NC1TaskAPI.DAL.Entities;
+
+namespace NC1TaskAPI.BLL.Repos.Interfaces;
+public interface IDepartmentRepo : IRepo<Department>
+{
+}
---
+++ Backend/NC1TaskAPI/NC1TaskAPI.BLL/Repos/Interfaces/IProgrammingLanguageRepo.cs
@@ -0,0 +1,7 @@
+﻿using NC1TaskAPI.BLL.Repos.Base;
+using NC1TaskAPI.DAL.Entities;
+
+namespace NC1TaskAPI.BLL.Repos.Interfaces;
+public interface IProgrammingLanguageRepo : IRepo<ProgrammingLanguage>
+{
+}
