c73f157883118641a2eab8cb681cfba055c7a7bd
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-13 18:19:12 +0300

    department implementation

--- Backend/NC1TaskAPI/NC1TaskAPI.DAL/Entities/Department.cs
+++ Backend/NC1TaskAPI/NC1TaskAPI.DAL/Entities/Department.cs
@@ -1,5 +1,28 @@
-﻿namespace NC1TaskAPI.DAL.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NC1TaskAPI.DAL.Entities;
 
 public class Department
 {
+    public Department()
+    {
+        Id = 0;
+        Name = string.Empty;
+        Floor = 1;
+    }
+
+    public Department(string name, int floor)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Floor = floor;
+    }
+
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MinLength(2)]
+    public string Name { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Floor { get; set; }
 }
