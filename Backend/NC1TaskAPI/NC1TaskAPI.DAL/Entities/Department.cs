c9b3d3dfab8809ca35b332d47b45d47948beb471
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-14 23:33:28 +0300

    removed constructors from other entities

--- Backend/NC1TaskAPI/NC1TaskAPI.DAL/Entities/Department.cs
+++ Backend/NC1TaskAPI/NC1TaskAPI.DAL/Entities/Department.cs
@@ -4,28 +4,13 @@ namespace NC1TaskAPI.DAL.Entities;
 
 public class Department
 {
-    public Department()
-    {
-        Id = 0;
-        Name = string.Empty;
-        Floor = 1;
-        Employees = new List<Employee>();
-    }
-
-    public Department(string name, int floor)
-    {
-        Name = name ?? throw new ArgumentNullException(nameof(name));
-        Floor = floor;
-        Employees = new List<Employee>();
-    }
-
     [Key]
     public int Id { get; set; }
     [Required]
     [MinLength(2)]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     [Required]
     [Range(0, int.MaxValue)]
     public int Floor { get; set; }
-    public List<Employee> Employees { get; set; }
+    public List<Employee> Employees { get; set; } = null!;
 }
--- Backend/NC1TaskAPI/NC1TaskAPI.DAL/Entities/ProgrammingLanguage.cs
+++ Backend/NC1TaskAPI/NC1TaskAPI.DAL/Entities/ProgrammingLanguage.cs
@@ -3,20 +3,10 @@
 namespace NC1TaskAPI.DAL.Entities;
 public class ProgrammingLanguage
 {
-    public ProgrammingLanguage()
-    {
-        Id = 0;
-        Name = string.Empty;
-    }
-    public ProgrammingLanguage(string name)
-    {
-        Name = name ?? throw new ArgumentNullException(nameof(name));
-    }
-
     [Key]
     public int Id { get; set; }
     [Required]
     [MinLength(2)]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public List<Employee> Employees { get; set; } = new();
 }
